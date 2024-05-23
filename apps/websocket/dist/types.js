e801685d55e0a5e3ca44febfc450540243f1362a
Mrinal Kumar <67271485+McACE007@users.noreply.github.com>
2024-05-23 12:50:36 +0530

    Update: Deleted apps/websocket/dist directory

--- apps/websocket/dist/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Type = void 0;
-var Type;
-(function (Type) {
-    Type[Type["FILE"] = 0] = "FILE";
-    Type[Type["DIRECTORY"] = 1] = "DIRECTORY";
-})(Type || (exports.Type = Type = {}));
-//# sourceMappingURL=types.js.map
