a3ec19850eae6e62a556335a0d788a8867db4277
McACE007 <mrinalace@gmail.com>
2024-06-14 21:44:25 +0530

    fix: change backend url

--- apps/website/src/config.ts
+++ apps/website/src/config.ts
@@ -1,2 +1,2 @@
-export const EXECUTION_ENGINE_URI = "http://ec2-3-0-51-125.ap-southeast-1.compute.amazonaws.com";
-export const HOSTED_URL = "http://ec2-3-0-51-125.ap-southeast-1.compute.amazonaws.com/output";
+export const EXECUTION_ENGINE_URI = "https://code-collab-backend-k0mt.onrender.com/";
+export const HOSTED_URL = "https://code-collab-backend-k0mt.onrender.com/output";
